041aa6527a19014fa32bbc979933d13b14198c6e
Dimitrii <sexykdi@gmail.com>
2019-07-24 13:51:40 +0300

    remove excess

--- vanilla_dijkstra_test.go
+++ vanilla_dijkstra_test.go
@@ -3,24 +3,15 @@ package ch
 import (
 	"log"
 	"testing"
-	"time"
 )
 
 func TestVanillaShortestPath(t *testing.T) {
 	g := Graph{}
 	graphFromCSV(&g, "data/pgrouting_osm.csv")
-	// log.Println("Please wait until contraction hierarchy is prepared")
-	// g.PrepareContracts()
 	log.Println("TestShortestPath is starting...")
 	u := 69618
 	v := 5924
-	st := time.Now()
-
 	ans, path := g.VanillaShortestPath(u, v)
-	// for i := 0; i < 300; i++ {
-	// 	ans, path = g.VanillaShortestPath(u, v)
-	// }
-	log.Println(time.Since(st))
 	if len(path) != 160 {
 		t.Errorf("Num of vertices in path should be 160, but got %d", len(path))
 	}
