b7a11c390a21da4d5894552e34b4a03e013e6785
Dimitrii <sexykdi@gmail.com>
2019-07-29 08:46:28 +0300

    d

--- export_test.go
+++ export_test.go
@@ -1,9 +1,72 @@
 package ch
 
 import (
+	"container/heap"
+	"fmt"
 	"log"
+	"math"
 	"testing"
 )
+
+type V struct {
+	from   int
+	to     int
+	weight float64
+}
+
+func TestRestriction(t *testing.T) {
+	vertices := []V{
+		V{1, 2, 1.0},
+		V{2, 3, 1.0},
+		V{3, 4, 1.0},
+		V{4, 5, 1.0},
+		V{5, 6, 1.0},
+		V{5, 7, 1.0},
+		V{2, 5, 1.0},
+		V{8, 2, 1.0},
+	}
+
+	graph := Graph{}
+
+	for i := range vertices {
+		graph.CreateVertex(vertices[i].from)
+		graph.CreateVertex(vertices[i].to)
+		graph.AddEdge(vertices[i].from, vertices[i].to, vertices[i].weight)
+	}
+
+	// Искуственный контракт
+	if _, ok := graph.contracts[0]; !ok {
+		graph.contracts[0] = make(map[int]int)
+		graph.contracts[0][4] = 1
+	}
+	graph.contracts[0][4] = 1
+
+	graph.Vertices[0].outEdges = append(graph.Vertices[0].outEdges, 4)
+	graph.Vertices[0].outECost = append(graph.Vertices[0].outECost, math.MaxFloat64)
+	graph.Vertices[4].inEdges = append(graph.Vertices[4].inEdges, 0)
+	graph.Vertices[4].inECost = append(graph.Vertices[4].inECost, math.MaxFloat64)
+
+	graph.computeImportance()
+	var extractNum int
+	for graph.pqImportance.Len() != 0 {
+		vertex := heap.Pop(graph.pqImportance).(*Vertex)
+		vertex.computeImportance()
+		if graph.pqImportance.Len() != 0 && vertex.importance > graph.pqImportance.Peek().(*Vertex).importance {
+			graph.pqImportance.Push(vertex)
+			continue
+		}
+		graph.Vertices[vertex.vertexNum].orderPos = extractNum
+		extractNum = extractNum + 1
+	}
+	graph.PrepareContracts()
+	cost, path := graph.VanillaShortestPath(1, 5)
+	fmt.Println(cost, path)
+
+	cost1, path1 := graph.ShortestPath(1, 5)
+	fmt.Println(cost1, path1)
+
+	t.Error("done")
+}
 
 func TestExport(t *testing.T) {
 	g := Graph{}
