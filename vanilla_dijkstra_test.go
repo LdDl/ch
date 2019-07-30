6d3afb9ad2e0a0875cec60fb3d84473e24556c63
Dimitrii <sexykdi@gmail.com>
2019-07-30 12:58:44 +0300

    tests for turn restrictions

--- vanilla_dijkstra_test.go
+++ vanilla_dijkstra_test.go
@@ -18,6 +18,7 @@ func TestVanillaShortestPath(t *testing.T) {
 	if Round(ans, 0.00005) != Round(19135.6581215226, 0.00005) {
 		t.Errorf("Length of path should be 19135.6581215226, but got %f", ans)
 	}
+	log.Println("TestVanillaShortestPath is Ok!")
 }
 
 func Round(x, unit float64) float64 {
