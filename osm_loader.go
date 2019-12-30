e23cec44448485552319e84d203d533507e4be73
Dimitrii <sexykdi@gmail.com>
2019-12-30 09:51:39 +0300

    trying to deal with expanded graph

--- osm_loader.go
+++ osm_loader.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math"
 	"os"
+	"time"
 
 	"github.com/paulmach/osm"
 	"github.com/paulmach/osm/osmpbf"
@@ -15,6 +16,11 @@ import (
 type restrictionComponent struct {
 	ID   int64
 	Type string
+}
+
+type expandedEdge struct {
+	ID   int64
+	Cost float64
 }
 
 // ImportFromOSMFile Imports graph from file of PBF-format (in OSM terms)
@@ -33,6 +39,10 @@ func ImportFromOSMFile(fileName string, cfg *OsmConfiguration) (*Graph, error) {
 	defer scanner.Close()
 
 	nodes := make(map[int64][2]float64)
+	vertices := make(map[int64]bool)
+	newEdges := make(map[int64]map[int64]expandedEdge)
+	newEdgeID := int64(1)
+
 	restrictions := make(map[restrictionComponent]map[restrictionComponent]restrictionComponent)
 
 	for scanner.Scan() {
@@ -58,11 +68,35 @@ func ImportFromOSMFile(fileName string, cfg *OsmConfiguration) (*Graph, error) {
 						a := Coord{Lon: nodes[source][0], Lat: nodes[source][1]}
 						b := Coord{Lon: nodes[target][0], Lat: nodes[target][1]}
 						cost := DistanceBetweenPoints(a, b) * 1000
-						graph.CreateVertex(source)
-						graph.CreateVertex(target)
-						graph.AddEdge(source, target, cost)
+
+						vertices[source] = true
+						vertices[target] = true
+
+						if _, ok := newEdges[source]; !ok {
+							newEdges[source] = make(map[int64]expandedEdge)
+						}
+						newEdges[source][target] = expandedEdge{
+							ID:   newEdgeID,
+							Cost: cost,
+						}
+						newEdgeID++
+
+						if source == 5107446176 {
+							fmt.Println("first", target)
+						}
+						// graph.CreateVertex(source)
+						// graph.CreateVertex(target)
+						// graph.AddEdge(source, target, cost)
 						if oneway == false {
-							graph.AddEdge(target, source, cost)
+							if _, ok := newEdges[target]; !ok {
+								newEdges[target] = make(map[int64]expandedEdge)
+							}
+							newEdges[target][source] = expandedEdge{
+								ID:   newEdgeID,
+								Cost: cost,
+							}
+							newEdgeID++
+							// graph.AddEdge(target, source, cost)
 						}
 					}
 				}
@@ -79,7 +113,7 @@ func ImportFromOSMFile(fileName string, cfg *OsmConfiguration) (*Graph, error) {
 				if len(members) != 3 {
 					fmt.Printf("Restriction does not contain 3 members, relation ID: %d", relation.ID)
 				}
-				log.Println(tag, members)
+				// log.Println(tag, members)
 				firstMember := restrictionComponent{-1, ""}
 				secondMember := restrictionComponent{-1, ""}
 				thirdMember := restrictionComponent{-1, ""}
@@ -139,12 +173,60 @@ func ImportFromOSMFile(fileName string, cfg *OsmConfiguration) (*Graph, error) {
 		}
 	}
 
-	for k, v := range restrictions {
-		fmt.Println(k)
-		for i, j := range v {
-			fmt.Println("\t", i, j)
+	expandedGraph := make(map[int64]map[int64]float64)
+	for source := range newEdges {
+		for target := range newEdges[source] {
+			sourceExpandVertex := newEdges[source][target]
+			sourceCost := sourceExpandVertex.Cost
+			if targetAsSource, ok := newEdges[target]; ok {
+				for subTarget := range targetAsSource {
+					targetExpandVertex := newEdges[target][subTarget]
+					targetCost := targetExpandVertex.Cost
+					if _, ok := expandedGraph[sourceExpandVertex.ID]; !ok {
+						expandedGraph[sourceExpandVertex.ID] = make(map[int64]float64)
+					}
+					// if target == 5107446176 {
+					// 	fmt.Println("second", source, target, subTarget, sourceExpandVertex.ID, targetExpandVertex.ID, sourceCost+targetCost)
+					// }
+					expandedGraph[sourceExpandVertex.ID][targetExpandVertex.ID] = (sourceCost + targetCost) / 2.0
+				}
+			}
 		}
 	}
+
+	source := int64(2574862283)
+	target := int64(5107446176)
+	subTarget := int64(96489258)
+
+	edge := newEdges[source][target]
+	subEdge := newEdges[target][subTarget]
+	// toTarget1 := graph.Vertices[15770].inEdges
+	// toTargetCost1 := graph.Vertices[15770].inECost[0]
+
+	fmt.Println(edge, subEdge, expandedGraph[edge.ID][subEdge.ID])
+
+	for source := range expandedGraph {
+		for target := range expandedGraph[source] {
+			cost := expandedGraph[source][target]
+			graph.CreateVertex(source)
+			graph.CreateVertex(target)
+			graph.AddEdge(source, target, cost)
+		}
+	}
+
+	log.Println("Number of edges:", len(newEdges))
+	log.Println("Number of vertices:", len(vertices))
+	log.Println("Number of new edges:", len(expandedGraph))
+	log.Println("Number of new vertices:", len(graph.Vertices))
+	log.Println("Number of restrictions:", len(restrictions))
+
+	st := time.Now()
+	graph.PrepareContracts()
+	log.Println("Elapsed to prepare contracts:", time.Since(st))
+	u := int64(17756)
+	v := int64(17757)
+	ans, path := graph.ShortestPath(u, v)
+	log.Println(ans, path)
 
 	scanErr := scanner.Err()
 	if scanErr != nil {
