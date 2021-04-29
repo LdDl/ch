28f03b16a814679c491a5d832f683394914ffef0
Dimitrii Lopanov <sexykdi@gmail.com>
2021-04-29 18:51:42 +0300

    add linear search/delete/update

--- vertex.go
+++ vertex.go
@@ -92,12 +92,6 @@ func (vertex *Vertex) bidirectedEdges() int {
 	return ans
 }
 
-// incidentEdge incident edge for certain vertex
-type incidentEdge struct {
-	vertexID int64
-	cost     float64
-}
-
 // Distance Information about contraction between source vertex and contraction vertex
 type Distance struct {
 	contractID  int64
@@ -116,4 +110,76 @@ func NewDistance() *Distance {
 		revDistance: math.MaxFloat64,
 		queryDist:   math.MaxFloat64,
 	}
+}
+
+// incidentEdge incident edge for certain vertex
+type incidentEdge struct {
+	vertexID int64
+	cost     float64
+}
+
+// findInIncidentEdge Returns index of incoming incident edge by vertex ID
+// If incoming incident edge is not found then this function returns -1
+func (vertex *Vertex) findInIncidentEdge(vertexID int64) int {
+	for i := range vertex.inIncidentEdges {
+		if vertex.inIncidentEdges[i].vertexID == vertexID {
+			return i
+		}
+	}
+	return -1
+}
+
+// findOutIncidentEdge Returns index of outcoming incident edge by vertex ID on the other side of that edge
+// If outcoming incident edge is not found then this function returns -1
+func (vertex *Vertex) findOutIncidentEdge(vertexID int64) int {
+	for i := range vertex.outIncidentEdges {
+		if vertex.outIncidentEdges[i].vertexID == vertexID {
+			return i
+		}
+	}
+	return -1
+}
+
+// updateInIncidentEdge Updates incoming incident edge's cost by vertex ID on the other side of that edge
+// If operation is not successful then this function returns False
+func (vertex *Vertex) updateInIncidentEdge(vertexID int64, cost float64) bool {
+	idx := vertex.findInIncidentEdge(vertexID)
+	if idx < 0 {
+		return false
+	}
+	vertex.inIncidentEdges[idx].cost = cost
+	return true
+}
+
+// updateOutIncidentEdge Updates outcoming incident edge's cost by vertex ID on the other side of that edge
+// If operation is not successful then this function returns False
+func (vertex *Vertex) updateOutIncidentEdge(vertexID int64, cost float64) bool {
+	idx := vertex.findOutIncidentEdge(vertexID)
+	if idx < 0 {
+		return false
+	}
+	vertex.outIncidentEdges[idx].cost = cost
+	return true
+}
+
+// deleteInIncidentEdge Deletes incoming incident edge by vertex ID on the other side of that edge
+// If operation is not successful then this function returns False
+func (vertex *Vertex) deleteInIncidentEdge(vertexID int64) bool {
+	idx := vertex.findInIncidentEdge(vertexID)
+	if idx < 0 {
+		return false
+	}
+	vertex.inIncidentEdges = append(vertex.inIncidentEdges[:idx], vertex.inIncidentEdges[idx+1:]...)
+	return true
+}
+
+// deleteOutIncidentEdge Deletes outcoming incident edge by vertex ID on the other side of that edge
+// If operation is not successful then this function returns False
+func (vertex *Vertex) deleteOutIncidentEdge(vertexID int64) bool {
+	idx := vertex.findOutIncidentEdge(vertexID)
+	if idx < 0 {
+		return false
+	}
+	vertex.outIncidentEdges = append(vertex.outIncidentEdges[:idx], vertex.outIncidentEdges[idx+1:]...)
+	return true
 }
