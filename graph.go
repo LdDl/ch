b6d27d05ac1f095cf5c329b4353788eb5e2b18a4
Mert Akman <mertakman2012@gmail.com>
2022-12-05 17:07:20 +0200

    add:preconditions for initializing vertices slice and label map

--- graph.go
+++ graph.go
@@ -96,8 +96,13 @@ func (graph *Graph) AddEdge(from, to int64, weight float64) error {
 }
 
 func (graph *Graph) BatchCreateVertex(vertexLabels []int64) error {
-	graph.Vertices = make([]Vertex, 0, len(vertexLabels))
-	graph.mapping = make(map[int64]int64, len(vertexLabels))
+	if len(graph.Vertices) == 0 {
+		graph.Vertices = make([]Vertex, 0, len(vertexLabels))
+	}
+
+	if len(graph.mapping) == 0 {
+		graph.mapping = make(map[int64]int64, len(vertexLabels))
+	}
 
 	for _, label := range vertexLabels {
 		if err := graph.CreateVertex(label); err != nil {
